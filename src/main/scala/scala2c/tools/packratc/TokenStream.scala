2a8ccededa2b533360c654b2ad173b401c35f874
Yichen Xu <yichen.x@outlook.com>
2021-03-04 16:39:28 +0800

    Update doc

--- src/main/scala/scala2c/tools/packratc/TokenStream.scala
+++ src/main/scala/scala2c/tools/packratc/TokenStream.scala
@@ -2,6 +2,20 @@ package scala2c.tools.packratc
 
 /** Interface for a token stream, as an input to the packrat parser.
  *
+ *  [[TokenStream.startBlock]] and [[TokenStream.endBlock]] should be called at the beginning and the end of a block.
+ *  For example:
+ *  ``` scala
+ *    {
+ *      val x = 1
+ *  //  ^ Called here. Push the current indent level of the block.
+ *      val y =
+ *  //  ^ Will produce a NewLine token.
+ *        1
+ *  //    ^ Will NOT produce a NewLine token. Assumed as the same line logically.
+ *    }
+ *  //^ End of block. Call end block here to pop the indent level.
+ *  ```
+ *  
  * @tparam T  The type of produced token.
  */
 trait TokenStream[T] {
@@ -11,25 +25,10 @@ trait TokenStream[T] {
 
   /** Push current position into the block indent stack.
    *
-   *  The function is called at the beginning of a block. For example:
-   *  
-   *  {{{
-   *    {
-   *      val x = 1
-   * //   ^ Called here. Push the current indent level of the block.
-   *      val y =
-   * //   ^ Will produce a NewLine token.
-   *        1
-   * //     ^ Will NOT produce a NewLine token. Assumed as the same line logically.
-   *    }
-   * // ^ End of block. Call end block here to pop the indent level.
-   *  }}}
    */
   def startBlock: Unit
 
   /** Pop the indent stack.
-   *
-   *  The usage has been illustrated in the example of [[TokenStream.startBlock]].
    *
    *  Will throw an error when it is called at the root indent level (indention = 0 at the begining of each source).
    */
