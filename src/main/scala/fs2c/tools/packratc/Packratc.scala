31f1326ddbfe708b5103babcdceed1f6fabe6b64
Yichen Xu <yichen.x@outlook.com>
2021-03-05 00:00:28 +0800

    Fix error in doc

--- src/main/scala/fs2c/tools/packratc/Packratc.scala
+++ src/main/scala/fs2c/tools/packratc/Packratc.scala
@@ -6,7 +6,7 @@ import fs2c.tools.packratc.syntax.ParserSyntax
  *  PEGs can describe a wide set of formal language, yet less powerful than CFGs.
  *  It is known that packrat parsers can parse PEGs in linear time, and it takes up extra
  *  memory space (for memoization) of O(N x S) where N is the number of symbols in the grammar,
- *  and S is the source length. Since the grammar is fixed, therefore the space complexity is also
+ *  and S is the source length. Since the grammar is fixed, the space complexity is also
  *  linear to the source size.
  */
 object Packratc extends ParserSyntax with ParserFunctions
