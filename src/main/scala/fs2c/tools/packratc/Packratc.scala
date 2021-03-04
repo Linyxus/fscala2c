10211755351d67c51237e90575b0f5f0c7029802
Yichen Xu <yichen.x@outlook.com>
2021-03-04 23:02:31 +0800

    Update Packrat companion object

--- src/main/scala/fs2c/tools/packratc/Packratc.scala
+++ src/main/scala/fs2c/tools/packratc/Packratc.scala
@@ -2,4 +2,8 @@ package fs2c.tools.packratc
 
 import fs2c.tools.packratc.syntax.ParserSyntax
 
-object Packratc extends ParserSyntax
+/** Packratc is a toolkit of packrat parser combinators for Parsing Expression Grammar (PEG).
+ *  PEGs can describe a wide set of formal language, yet less powerful than CFGs.
+ *  It is known that packrat parsers can parse PEGs in linear time.
+ */
+object Packratc extends ParserSyntax with ParserFunctions
