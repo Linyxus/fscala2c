939940037c27e1314c718d4e2354f9c78edf1065
Yichen Xu <yichen.x@outlook.com>
2021-03-04 22:53:20 +0800

    Add tool functions for LazyList

---
+++ src/main/scala/scala2c/tools/data/LazyListTools.scala
@@ -0,0 +1,14 @@
+package scala2c.tools.data
+
+trait LazyListTools {
+  def fromList[X](xs: List[X]): LazyList[X] = {
+    @annotation.tailrec def recur(xs: List[X], acc: LazyList[X]): LazyList[X] = xs match {
+      case Nil => acc
+      case x :: xs => recur(xs, x #:: acc)
+    }
+    
+    recur(xs.reverse, LazyList())
+  }
+}
+
+object LazyListTools extends LazyListTools
