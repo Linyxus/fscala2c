6ca07f0b6253ca7abd0a1f8eedc62b79d941f456
Yichen Xu <yichen.x@outlook.com>
2021-03-04 23:59:26 +0800

    Update doc of Packratc

--- src/main/scala/fs2c/tools/packratc/Packratc.scala
+++ src/main/scala/fs2c/tools/packratc/Packratc.scala
@@ -4,6 +4,9 @@ import fs2c.tools.packratc.syntax.ParserSyntax
 
 /** Packratc is a toolkit of packrat parser combinators for Parsing Expression Grammar (PEG).
  *  PEGs can describe a wide set of formal language, yet less powerful than CFGs.
- *  It is known that packrat parsers can parse PEGs in linear time.
+ *  It is known that packrat parsers can parse PEGs in linear time, and it takes up extra
+ *  memory space (for memoization) of O(N x S) where N is the number of symbols in the grammar,
+ *  and S is the source length. Since the grammar is fixed, therefore the space complexity is also
+ *  linear to the source size.
  */
 object Packratc extends ParserSyntax with ParserFunctions
