7637ef371f4a4fdef819bf51268a8e8d5e3c285d
Yichen Xu <yichen.x@outlook.com>
2021-03-04 15:36:49 +0800

    Add TokenStream abstract type

--- src/main/scala/scala2c/tools/packratc/TokenStream.scala
+++ src/main/scala/scala2c/tools/packratc/TokenStream.scala
@@ -1,5 +1,35 @@
 package scala2c.tools.packratc
 
-trait TokenStream {
+/** Interface for a token stream, as an input to the packrat parser.
+ * 
+ * @tparam T  The type of produced token.
+ */
+trait TokenStream[T] {
+  /** Get the next token.
+   */
+  def nextToken: T
 
+  /** Push current position into the block indent stack.
+   * 
+   *  The function is called at the beginning of a block. For example:
+   *  
+   *    {
+   *      val x = 1
+   *      ^ Called here. Push the current indent level of the block.
+   *      val y =
+   *      ^ Will produce a NewLine token.
+   *        1
+   *        ^ Will NOT produce a NewLine token. Assumed as the same line logically.
+   *    }
+   *    ^ End of block. Call end block here to pop the indent level.
+   */
+  def startBlock: Unit
+
+  /** Pop the indent stack.
+   * 
+   *  The usage has been illustrated in the example of [[TokenStream.startBlock]].
+   *  
+   *  Will throw an error when it is called at the root indent level (indention = 0 at the begining of each source).
+   */
+  def endBlock: Unit
 }
