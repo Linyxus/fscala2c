3d3625659d589ceabd2318035bf6e546eed84cd0
Yichen Xu <yichen.x@outlook.com>
2021-03-06 00:39:18 +0800

    Add expression trees

--- src/main/scala/fs2c/ast/fs/Trees.scala
+++ src/main/scala/fs2c/ast/fs/Trees.scala
@@ -15,9 +15,9 @@ trait Trees {
   /** A tag for Class values. Defined classes like ClassDef or builtin types.
    */
   trait ClassValue
-  
+
   /** A class definition.
-   *  
+   *
    *  ```scala
    *  class Foo extends Bar {
    *    // members
@@ -38,6 +38,25 @@ trait Trees {
    */
   sealed trait Expr[F[_]]
 
+  /** Lambda expressions.
+   */
+  case class LambdaExpr[F[_]](params: List[Symbol[LambdaParam[F]]], ascript: Option[Type], body: F[Expr[F]]) extends Expr[F]
+  /** Parameter in the lambda expression.
+   */
+  case class LambdaParam[F[_]](sym: Symbol[LambdaParam[F]], ascript: Option[Type], lambda: F[LambdaExpr[F]])
+
+  /** Block expressions.
+   */
+  case class BlockExpr[F[_]](defs: List[F[LocalDef[F]]], expr: F[Expr[F]]) extends Expr[F]
+
+  /** Local definitions in block expressions.
+   */
+  enum LocalDef[F[_]] {
+    case Val[F[_]](sym: Symbol[Val[F]], ascript: Option[Type], body: F[Expr[F]]) extends LocalDef[F]
+    case Var[F[_]](sym: Symbol[Val[F]], ascript: Option[Type], body: F[Expr[F]]) extends LocalDef[F]
+    case Eval[F[_]](body: F[Expr[F]]) extends LocalDef[F]
+  }
+
   /** Type projectors to embed extra information into the tree.
    *  Currently placeholders.
    */
@@ -47,7 +66,7 @@ trait Trees {
   /** Transform tree type to Untyped tree type. For more information, see [[Untyped]].
    */
   type UntypedTree = [T[_[_]]] =>> Untyped[T[Untyped]]
-  
+
   /** Transform tree type to Typed tree type. For more information, see [[Typed]].
    */
   type TypedTree = [T[_[_]]] =>> Typed[T[Typed]]
