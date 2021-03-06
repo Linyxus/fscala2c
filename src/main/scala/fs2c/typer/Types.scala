ede110c0e3163b72a5e06994551f4e51a2ca14d5
Yichen Xu <yichen.x@outlook.com>
2021-03-06 21:21:58 +0800

    Add GroundType for built-in types

--- src/main/scala/fs2c/typer/Types.scala
+++ src/main/scala/fs2c/typer/Types.scala
@@ -1,9 +1,29 @@
 package fs2c.typer
 
-trait Types {
+import fs2c.ast.Symbol
 
+trait Types {
+  /** The trait for all types.
+    */
   trait Type
 
+  /** A proxy type referring to type through a symbol.
+    */
+  case class SymbolType(refSym: Symbol.Ref) extends Type
+
+  /** Ground types.
+    */
+  enum GroundType extends Type {
+    case IntType
+    case FloatType
+    case BooleanType
+    case StringType
+    case ArrayType(itemType: Type)
+  }
+
+  /** Lambda types.
+    */
+  case class LambdaType(paramTypes: List[Type], valueType: Type) extends Type
 }
 
 object Types extends Types
