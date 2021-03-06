4b3c2ab265ca37e4645c966de8f539f882da4bfd
Yichen Xu <yichen.x@outlook.com>
2021-03-06 21:21:08 +0800

    Seperate Symbol into Symbol value and Symbol.Ref

--- src/main/scala/fs2c/ast/Symbol.scala
+++ src/main/scala/fs2c/ast/Symbol.scala
@@ -1,5 +1,23 @@
 package fs2c.ast
 
-/** A symbol with `name` and reference to a value of `T`.
+/** A symbol in the Scala code.
+  * 
+  * @param name Symbol name.
+  * @param dealias Reference contained in the symbol.
+  * @tparam T Referenced type.
   */
-case class Symbol[T](name: String, ref: T)
+case class Symbol[T](name: String, var dealias: T)
+
+object Symbol {
+  enum Ref {
+    case Unresolved(symName: String)
+    case Resolved[T](sym: Symbol[T])
+    
+    def isResolved: Boolean = this match {
+      case _ : Unresolved => false
+      case _ => true
+    }
+    
+    def nonResolved: Boolean = !isResolved
+  }
+}
