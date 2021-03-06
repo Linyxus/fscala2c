caf9b1e841ecf3b23183d3ad9b69b37cedaf4edf
Yichen Xu <yichen.x@outlook.com>
2021-03-06 21:21:50 +0800

    Add untpd Trees

--- src/main/scala/fs2c/ast/fs/Trees.scala
+++ src/main/scala/fs2c/ast/fs/Trees.scala
@@ -12,9 +12,9 @@ import fs2c.typer.Types._
   */
 trait Trees {
 
-  /** A tag for Class values. Defined classes like ClassDef or builtin types.
+  /** The trait for all terms living in the value domain.
     */
-  trait ClassValue
+  trait Term
 
   /** A class definition.
     *
@@ -28,51 +28,66 @@ trait Trees {
     * ClassDef(sym = Foo, parent = Some(Bar), List(...))
     * ```
     */
-  case class ClassDef[F[_]](sym: Symbol[ClassDef[F]], parent: Option[Symbol[ClassValue]], members: List[F[MemberDef[F]]]) extends ClassValue
+  case class ClassDef[F[_]](sym: Symbol[F[ClassDef[F]]], parent: Option[Symbol[F[ClassDef[F]]]], members: List[F[MemberDef[F]]]) extends Type
 
   /** A member definition in the class body.
     */
-  case class MemberDef[F[_]](sym: Symbol[ClassDef[F]], mutable: Boolean, ascript: Option[Type], body: F[Expr[F]])
+  case class MemberDef[F[_]](sym: Symbol[ClassDef[F]], mutable: Boolean, ascript: Option[Type], body: F[Expr[F]]) extends Term
 
   /** Tree for expressions
     */
-  sealed trait Expr[F[_]]
+  sealed trait Expr[F[_]] extends Term
 
   /** Lambda expressions.
     */
-  case class LambdaExpr[F[_]](params: List[Symbol[LambdaParam[F]]], ascript: Option[Type], body: F[Expr[F]]) extends Expr[F]
+  case class LambdaExpr[F[_]](params: List[Symbol[LambdaParam]], tpe: Option[Type], body: F[Expr[F]]) extends Expr[F]
 
   /** Parameter in the lambda expression.
     */
-  case class LambdaParam[F[_]](sym: Symbol[LambdaParam[F]], ascript: Option[Type], lambda: F[LambdaExpr[F]])
+  case class LambdaParam(sym: Symbol[LambdaParam], tpe: Type)
 
   /** Block expressions.
     */
   case class BlockExpr[F[_]](defs: List[F[LocalDef[F]]], expr: F[Expr[F]]) extends Expr[F]
 
+  /** Identifier that refers to a symbol.
+    */
+  case class IdentifierExpr[F[_]](sym: Symbol.Ref) extends Expr[F]
+
   /** Local definitions in block expressions.
     */
   enum LocalDef[F[_]] {
-
-    case Val[F[_]](sym: Symbol[Val[F]], ascript: Option[Type], body: F[Expr[F]]) extends LocalDef[F]
-
-    case Var[F[_]](sym: Symbol[Val[F]], ascript: Option[Type], body: F[Expr[F]]) extends LocalDef[F]
-
+    case Val[F[_]](sym: Symbol[Val[F]], tpe: Option[Type], body: F[Expr[F]]) extends LocalDef[F]
+    case Var[F[_]](sym: Symbol[Val[F]], tpe: Option[Type], body: F[Expr[F]]) extends LocalDef[F]
     case Eval[F[_]](body: F[Expr[F]]) extends LocalDef[F]
-
   }
 
   /** Type projectors to embed extra information into the tree.
     * Currently placeholders.
     */
-  type Untyped =[X] =>> X
+  case class Untyped[X](tree: X)
   type Typed =[X] =>> X
 
   /** Transform tree type to Untyped tree type. For more information, see [[Untyped]].
     */
-  type UntypedTree =[T[_[_]]] =>> Untyped[T[Untyped]]
+  type UntypedTree = [T[_[_]]] =>> Untyped[T[Untyped]]
 
   /** Transform tree type to Typed tree type. For more information, see [[Typed]].
     */
-  type TypedTree =[T[_[_]]] =>> Typed[T[Typed]]
+  type TypedTree = [T[_[_]]] =>> Typed[T[Typed]]
+
+  /** Untyped trees.
+    */
+  object untpd {
+    type Expr = UntypedTree[Trees.Expr]
+    
+    type LambdaExpr = UntypedTree[Trees.LambdaExpr]
+    
+    type BlockExpr = UntypedTree[Trees.BlockExpr]
+    type LocalDef = UntypedTree[Trees.LocalDef]
+    
+    type IdentifierExpr = UntypedTree[Trees.IdentifierExpr]
+  }
 }
+
+object Trees extends Trees
