084ca0812cff21b1cc0a27082f41b5b507b0309d
Yichen Xu <yichen.x@outlook.com>
2021-03-16 00:45:26 +0800

    Add general testcase tests/point.scala

---
+++ tests/point.scala
@@ -0,0 +1,22 @@
+class Point(x0 : Int, y0 : Int) {
+  val x = x0
+  val y = y0
+  
+  val abs = (i : Int) => if i < 0 then -i else i
+  
+  val distTo = (other : Point) => {
+    val dx = abs(x - other.x)
+    val dy = abs(y - other.y)
+    dx + dy
+  }
+  
+  val distToOrigin = distTo(new Point(0, 0))
+}
+
+class Main {
+  val p1 = new Point(1, 1)
+  val p2 = new Point(3, 4)
+  
+  val d1 = p1.distTo(p2)
+  val d2 = p1.distToOrigin
+}
