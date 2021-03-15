f8921739d92f62d6810d6723791dee0f0aa46ff5
Yichen Xu <yichen.x@outlook.com>
2021-03-16 01:31:44 +0800

    Add fromPath factory function for ScalaSource

--- src/main/scala/fs2c/io/ScalaSource.scala
+++ src/main/scala/fs2c/io/ScalaSource.scala
@@ -1,5 +1,14 @@
 package fs2c.io
 
+import io.Source
+
 case class ScalaSource(path: String, content: String)
 
-object ScalaSource
+object ScalaSource {
+  def fromPath(path: String): ScalaSource = {
+    val f = Source.fromFile(path)
+    val src = ScalaSource(path, f.getLines() mkString "\n")
+    f.close()
+    src
+  }
+}
