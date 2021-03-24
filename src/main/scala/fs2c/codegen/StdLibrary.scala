6a554e0dd6c86668e4d5f86e5f1026d8465416e6
Yichen Xu <yichen.x@outlook.com>
2021-03-24 11:29:03 +0800

    Add func_closure library definition

--- src/main/scala/fs2c/codegen/StdLibrary.scala
+++ src/main/scala/fs2c/codegen/StdLibrary.scala
@@ -1,20 +1,50 @@
 package fs2c.codegen
 
-class StdLibrary {
+import fs2c.ast.c.{Trees => C}
+
+class StdLibrary(val gen: CodeGen) {
+  import StdLibrary.LibBundle
+
+  /** Structure for function closure.
+    * ```c
+    * struct func_closure {
+    *   void *func;
+    *   void *env;
+    * }
+    * ```
+    */
+  val FuncClosure: LibBundle = new LibBundle(gen) {
+    override protected def bundle: C.Definition =
+      C.StructDef.makeStructDef(
+        name = "func_closure",
+        memberDefs = List(
+          "func" :: defn.VoidPointer,
+          "env" :: defn.VoidPointer
+        )
+      )
+  }
 
 }
 
 object StdLibrary {
-  trait LibBundle[T] {
+  trait LibBundle(gen: CodeGen) {
     private var loaded: Boolean = false
     
     def isLoaded: Boolean = loaded
     
-    def load: T = {
-      loaded = true
-      bundle
-    }
+    def load: C.Definition = 
+      if loaded then
+        bundle
+      else {
+        loaded = true
+        dependencies foreach { dep => dep.load }
+        val d = bundle
+        gen.outDef(d)
+        d
+      }
     
-    protected def bundle: T
+    protected def bundle: C.Definition
+    
+    protected def dependencies: List[LibBundle] = Nil
   }
 }
