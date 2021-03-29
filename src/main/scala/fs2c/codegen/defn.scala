6dd2f7472f498151bf31ed84a6a2f2e249acc165
Yichen Xu <yichen.x@outlook.com>
2021-03-29 09:33:37 +0800

    Add tool functions in defn

--- src/main/scala/fs2c/codegen/defn.scala
+++ src/main/scala/fs2c/codegen/defn.scala
@@ -1,9 +1,11 @@
 package fs2c.codegen
 
-import fs2c.ast.c.{ Trees => C }
+import fs2c.ast.c.{Trees => C}
 import fs2c.ast.Symbol
+import fs2c.codegen.{ CodeBundles => bd }
 
 object defn {
+  import GroundFuncs._
   /** C type: (void *)
     */
   val VoidPointer = C.PointerType(C.VoidType)
@@ -17,9 +19,24 @@ object defn {
     (d.sym, block)
   }
 
-  def assignVar(sym: Symbol[C.VariableDef], expr: C.Expr): C.Statement =
-    C.Statement.AssignVar(sym, expr)
+  def assignVar(binding: C.Binding, expr: C.Expr): C.Statement =
+    C.Statement.AssignVar(binding, expr)
+    
+  def assignMember(v: C.Binding, d: Symbol[C.StructMember], expr: C.Expr): C.Statement =
+    v.getType match {
+      case C.StructType(structSym) =>
+        assert(structSym eq d.dealias.struct, "the designator should have the same struct type with the variable")
+        C.Statement.AssignMember(v, d, expr)
+      case _ =>
+        assert(false, "variable should be a struct")
+    }
   
   def funcType(paramTypes: List[C.Type], valueType: C.Type): C.FuncType =
     C.FuncType(valueType, paramTypes)
+    
+  def allocStruct(structDef: C.StructDef): C.Expr = malloc $$ C.SizeOf(structDef.tp)
+    
+  object GroundFuncs {
+    val malloc = C.GroundFunc("malloc", List("stdlib.h"))
+  }
 }
