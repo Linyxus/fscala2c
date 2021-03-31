99bbc656c68293fd31b3e3902f903f6c78faea8d
Yichen Xu <yichen.x@outlook.com>
2021-03-31 11:16:11 +0800

    Refactor printing instance for Expr

--- src/main/scala/fs2c/printing/printing.scala
+++ src/main/scala/fs2c/printing/printing.scala
@@ -74,23 +74,88 @@ object printing {
       def print(t: C.VoidType.type)(using printer: Printer) = printer.print("void")
     }
 
+    /** Showing an expression.
+      * 
+      * Binding level of expressions.
+      * 
+      * -1 : Starting
+      * 0  : &&, ||
+      * 1  : ==, !=, >, <, >=, <=
+      * 2  : +, -
+      * 3  : *, /
+      * 4  : ^
+      * 10 : unary -, !
+      * 15 : select, apply
+      * 20 : Identifier
+      */
+    def showExpr(expr: C.Expr): String = {
+      // binary operator priority
+      def bopPriority: Map[C.BinOpType, Int] = Map(
+        C.BinOpType.&& -> 0,
+        C.BinOpType.|| -> 0,
+        C.BinOpType.== -> 1,
+        C.BinOpType.!= -> 1,
+        C.BinOpType.>= -> 1,
+        C.BinOpType.<= -> 1,
+        C.BinOpType.> -> 1,
+        C.BinOpType.< -> 1,
+        C.BinOpType.+ -> 2,
+        C.BinOpType.- -> 2,
+        C.BinOpType.* -> 3,
+        C.BinOpType./ -> 3,
+        C.BinOpType.^ -> 4,
+      )
+      
+      // unary operator priority
+      def uopPrioriy: Map[C.UnaryOpType, Int] = Map(
+        C.UnaryOpType.- -> 10,
+        C.UnaryOpType.! -> 10
+      )
+      
+      val selectAndApplyLevel: Int = 15
+      val identLevel: Int = 20
+      
+      def go(expr: C.Expr, currentLevel: Int): String = expr match {
+        case C.BinOpExpr(op, e1, e2) =>
+          val nextLevel = bopPriority(op)
+          val s1 = go(e1, nextLevel)
+          val s2 = go(e2, nextLevel)
+          val s = s"$s1 $op $s1"
+          
+          if nextLevel < currentLevel then
+            s"($s)"
+          else
+            s
+        case C.UnaryOpExpr(op, e) =>
+          val nextLevel = uopPrioriy(op)
+          val s = s"$op ${go(e, nextLevel)}"
+          
+          if nextLevel < currentLevel then
+            s"($s)"
+          else
+            s
+        case C.CallFunc(func, params) =>
+          val sf = go(func, currentLevel = selectAndApplyLevel)
+          val sparams = params map { x => go(x, currentLevel = -1) } 
+          
+          s"$sf(${sparams mkString ", "})"
+        case C.SelectExpr(expr, designator) =>
+          val s = go(expr, currentLevel = selectAndApplyLevel)
+          
+          s"$s.$designator"
+        case C.IdentifierExpr(sym) =>
+          sym.name
+        case C.IntExpr(value) => value.toString
+        case C.BoolExpr(value) =>
+          if value then "1" else "0"
+        case C.FloatExpr(value) => value.toString
+      }
+      
+      go(expr, -1)
+    }
+
     given expr: Printing[C.Expr] = new Printing[C.Expr] {
-      def print(t: C.Expr)(using printer: Printer) = ???
-    }
-
-    val binOpExpr: Printing[C.BinOpExpr] = new Printing[C.BinOpExpr] {
-      def print(t: C.BinOpExpr)(using printer: Printer) = {
-        printer.inParen { expr.print(t.e1) }
-        printer.print(s" ${t.op} ")
-        printer.inParen { expr.print(t.e2) }
-      }
-    }
-
-    val unaryOpExpr: Printing[C.UnaryOpExpr] = new Printing[C.UnaryOpExpr] {
-      def print(t: C.UnaryOpExpr)(using printer: Printer) = {
-        printer.print(s" ${t.op} ")
-        printer.inParen { expr.print(t.e) }
-      }
+      def print(t: C.Expr)(using printer: Printer) = printer.print(showExpr(t))
     }
   }
 }
