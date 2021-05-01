c95ccaf2409cfc5b03399b3bb941144247b69bb1
Yichen Xu <yichen.x@outlook.com>
2021-05-01 17:30:12 +0800

    Support outputing; ignore output file

--- src/main/scala/fs2c/Main.scala
+++ src/main/scala/fs2c/Main.scala
@@ -1,10 +1,18 @@
 package fs2c
 
 import scopt.OParser
+import java.io.{File, FileWriter, BufferedWriter}
 
-case class CompilerConfig(sourcePath: String = "<empty>")
+case class CompilerConfig(sourcePath: String = "<empty>", outputPath: Option[String] = None)
 
 object Main {
+  def outputFile(path: String, content: String): Unit = {
+    val f = new File(path)
+    val bw = new BufferedWriter(new FileWriter(f))
+    bw.write(content)
+    bw.close()
+  }
+
   def main(args: Array[String]): Unit = {
     val builder = OParser.builder[CompilerConfig]
     val parser1 = {
@@ -15,17 +23,25 @@ object Main {
         opt[String]('s', "source")
           .required()
           .action((x, c) => c.copy(sourcePath = x))
-          .text("source file path")
+          .text("source file path"),
+        opt[String]('o', "output")
+          .action((x, c) => c.copy(outputPath = Some(x)))
+          .text("output file path")
       )
     }
 
     OParser.parse(parser1, args, CompilerConfig()) match {
-      case Some(CompilerConfig(sourcePath)) =>
+      case Some(CompilerConfig(sourcePath, outputPath)) =>
         val compiler = new Compiler
         val defs = compiler.typedFile(sourcePath)
         val (cDefs, includes) = compiler.genClassDefs(defs)
         val cSource = compiler.outputCode(cDefs, includes)
         println(cSource)
+
+        outputPath foreach { outputPath =>
+          println("writing to output file ...")
+          outputFile(outputPath, cSource)
+        }
       case _ =>
     }
   }
