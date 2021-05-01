42aa801acb494c4e89ac765dc3ce266730515d13
Yichen Xu <yichen.x@outlook.com>
2021-05-01 20:28:33 +0800

    Remove braces in Fibonacci CPS example

--- tests/fibo_cps.scala
+++ tests/fibo_cps.scala
@@ -3,9 +3,8 @@ class Main {
   val fibonacci: (Int, Int => Int) => Int = (n: Int, callback: Int => Int) =>
     if n <= 1 then
       callback(1)
-    else {
+    else
       fibonacci(n - 1, (t1: Int) => fibonacci(n - 2, (t2: Int) => callback(t1 + t2)))
-    }
 
   val identity = (x: Int) => x
 
