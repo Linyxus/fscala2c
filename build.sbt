3b37befaec675db116442986cdb05e69507f386b
Yichen Xu <yichen.x@outlook.com>
2021-05-01 15:59:39 +0800

    Add argument parser for main program

--- build.sbt
+++ build.sbt
@@ -8,5 +8,8 @@ lazy val root = project
 
     scalaVersion := scala3Version,
 
-    libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % "test"
+    libraryDependencies ++= Seq(
+      "com.novocode" % "junit-interface" % "0.11" % "test",
+      "com.github.scopt" %% "scopt" % "4.0.1",
+    )
   )
--- src/main/scala/fs2c/Main.scala
+++ src/main/scala/fs2c/Main.scala
@@ -1,5 +1,34 @@
 package fs2c
 
-object Main extends App {
-  println("Welcome to fscala2c")
+import scopt.OParser
+
+case class CompilerConfig(sourcePath: String = "<empty>")
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val builder = OParser.builder[CompilerConfig]
+    val parser1 = {
+      import builder._
+      OParser.sequence(
+        programName("fscala2c"),
+        head("fscala2c", "0.1.0"),
+        opt[String]('s', "source")
+          .required()
+          .action((x, c) => c.copy(sourcePath = x))
+          .text("source file path")
+      )
+    }
+
+    println(args.toList)
+
+    OParser.parse(parser1, args, CompilerConfig()) match {
+      case Some(CompilerConfig(sourcePath)) =>
+        val compiler = new Compiler
+        val defs = compiler.typedFile(sourcePath)
+        val (cDefs, includes) = compiler.genClassDefs(defs)
+        val cSource = compiler.outputCode(cDefs, includes)
+        println(cSource)
+      case _ =>
+    }
+  }
 }
