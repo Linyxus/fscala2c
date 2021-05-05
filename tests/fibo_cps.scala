59fba0f6a93bd30985c6d4105601cf63e29a0a69
Yichen Xu <yichen.x@outlook.com>
2021-05-05 20:53:10 +0800

    Update fibo_cps.scala testcase to use the new member method syntax

--- tests/fibo_cps.scala
+++ tests/fibo_cps.scala
@@ -1,14 +1,14 @@
 
 class Main {
-  val fibonacci: (Int, Int => Int) => Int = (n: Int, callback: Int => Int) =>
+  def fibonacci(n: Int, callback: Int => Int): Int =
     if n <= 1 then
       callback(1)
     else
       fibonacci(n - 1, (t1: Int) => fibonacci(n - 2, (t2: Int) => callback(t1 + t2)))
 
-  val identity = (x: Int) => x
+  def identity(x: Int) = x
 
-  val main = () => {
+  def main = {
     val n = readInt()
     var i = 0
     while i <= n do {
