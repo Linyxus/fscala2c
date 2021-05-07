b63a759a9ab3358d87067f160b03444c6edbeca0
Yichen Xu <yichen.x@outlook.com>
2021-05-07 16:51:37 +0800

    Update fibo_cps.scala example

--- tests/fibo_cps.scala
+++ tests/fibo_cps.scala
@@ -1,18 +1,16 @@
 
 class Main {
-  def fibonacci(n: Int, callback: Int => Int): Int =
+  def fibonacci(n: Int, callback: Int => Int) =
     if n <= 1 then
       callback(1)
     else
       fibonacci(n - 1, (t1: Int) => fibonacci(n - 2, (t2: Int) => callback(t1 + t2)))
 
-  def identity(x: Int) = x
-
   def main = {
     val n = readInt()
     var i = 0
     while i <= n do {
-      val f = fibonacci(i, identity)
+      val f = fibonacci(i, (x: Int) => x)
       printf("fibonacci(%d) = %d\n", i, f)
       i = i + 1
     }
